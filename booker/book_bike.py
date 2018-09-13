94aec324d4724532b71f104458350f0c9417240a
Theo <theogz@users.noreply.github.com>
2018-09-13 07:00:48 -0700

    Allow for async database usage (#11)
    * :bug: only pass booking_id to new thread
    
    * :wrench: only warn on non existing rental

--- booker/book_bike.py
+++ booker/book_bike.py
@@ -195,7 +195,7 @@ def cancel_rental():
                 'category': 'success'
             }
         else:
-            logger.error(
+            logger.warn(
                 f'{r.status_code} - {json.loads(r.text)}'
             )
             return {
@@ -210,7 +210,8 @@ def cancel_rental():
     }
 
 
-def schedule_trip(booking, email):
+def schedule_trip(booking_id, email):
+    booking = db.session.query(Bookings).get(booking_id)
 
     # Todo: handle auto-booking
     if booking.status == 'error':
